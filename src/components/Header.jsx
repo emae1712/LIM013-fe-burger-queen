e5e48f527c546331ab718a79d50ac31e77a30553
emae1712 <melissa.aechegaray@gmail.com>
2021-01-06 09:11:54 -0500

    starting with components

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Header = () =>(
+  <header>
+    <nav>
+      <ul>
+        <li><a href= "#home"> Astroburger</a></li>
+        <li>Mesas</li>
+        <li>Menu</li>
+        <li>Cocina</li>
+        <li>Progreso</li>
+      </ul>
+    </nav> 
+  </header>
+
+);
+
+export default Header;

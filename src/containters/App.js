7e24014117eee6d1a492781f4e31f7fe5e7aa089
emae1712 <melissa.aechegaray@gmail.com>
2021-01-07 16:34:28 -0500

    componentes and container directories

---
+++ src/containters/App.js
@@ -0,0 +1,15 @@
+
+import '../styles/App.css';
+import Header from '../components/Header'
+import Menu from '../components/Menu_options'
+
+function App() {
+  return (
+    <section className ='App'>
+      <Header/>
+      <Menu/>
+    </section>
+  );
+}
+
+export default App;

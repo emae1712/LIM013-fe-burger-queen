de7d5b5a7ba05de1fab9671b87de9d12aef06673
emae1712 <melissa.aechegaray@gmail.com>
2021-01-07 16:37:20 -0500

    boilerplate adding directories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containters/App';
+import App from './containter/App';
 import reportWebVitals from './reportWebVitals';
 import './firebase';
 

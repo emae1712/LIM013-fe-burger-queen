4c468c5bb06bbed894a24a4a97fc0088086e01e1
emae1712 <melissa.aechegaray@gmail.com>
2021-01-08 13:22:46 -0500

    starting with sass

--- src/container/App.js
+++ src/container/App.js
@@ -2,13 +2,15 @@
 import '../styles/App.css';
 import Header from '../components/Header'
 import Menu from '../components/Menu_categories'
+import Order from '../components/Order';
 
 function App() {
   return (
-    <section className ='App'>
+    <div className = "App">
       <Header/>
       <Menu/>
-    </section>
+      <Order/>
+    </div>
   );
 }
 
